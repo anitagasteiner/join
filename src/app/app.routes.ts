1054fa22492979a2a0c159bba855be18a7f000fc
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-03-27 20:50:44 +0100

    routerlink implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { SummaryComponent } from './components/summary/summary.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
 
 
 export const routes: Routes = [
-    { path: '', component: SummaryComponent }
+    { path: '', component: SummaryComponent },
+    { path: 'add-task', component: AddTaskComponent }
 ];
