50eac2cef68d919ac64bc73fb73b18e960c6b12d
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-03-28 17:26:23 +0100

    board component started // find and add task

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { SummaryComponent } from './components/summary/summary.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
+import { BoardComponent } from './components/board/board.component';
 
 
 export const routes: Routes = [
     { path: '', component: SummaryComponent },
-    { path: 'add-task', component: AddTaskComponent }
+    { path: 'add-task', component: AddTaskComponent },
+    { path: 'board', component: BoardComponent }
 ];
