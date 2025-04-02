b08aed8ccbf7323d81c042f737eb4ec59d03d486
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-04-02 09:23:33 +0200

    contacts component added to routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { SummaryComponent } from './components/summary/summary.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
 import { BoardComponent } from './components/board/board.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
 
 
 export const routes: Routes = [
     { path: '', component: SummaryComponent },
     { path: 'add-task', component: AddTaskComponent },
-    { path: 'board', component: BoardComponent }
+    { path: 'board', component: BoardComponent },
+    { path: 'contacts', component: ContactsComponent }
 ];
