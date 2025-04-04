19462d139bd9f5929eeec5dc280794f8fccf6d71
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-04-04 22:25:23 +0200

    contact added notification

--- src/app/components/contacts/add-contact/add-contact.component.ts
+++ src/app/components/contacts/add-contact/add-contact.component.ts
@@ -25,9 +25,7 @@ export class AddContactComponent {
       phone: ""
   };
 
-  isLoading = false;
-  successMessage = '';
-  errorMessage = '';
+  contactAdded: boolean = false;
 
   @ViewChild('contactForm') contactForm!: NgForm; // Zugriff auf das Formular
 
@@ -41,26 +39,22 @@ export class AddContactComponent {
   }
 
   async onSubmit(form: NgForm) {
-    console.log('onSubmit wurde aufgerufen');
     if (form.invalid) {
-      console.log('form invalid');
-      this.errorMessage = 'Bitte füllen Sie alle Pflichtfelder aus.';
+      // this.errorMessage = 'Bitte füllen Sie alle Pflichtfelder aus.';
       return;
     }
-    this.isLoading = true;
-    this.successMessage = '';
-    this.errorMessage = '';
     try {
       console.log('Speichere Kontakt:', this.newContact);
       await this.dataBaseService.addData('contacts', this.newContact); // 'contacts' als Sammlungsname
-      this.successMessage = 'Kontakt erfolgreich gespeichert!';
       form.resetForm();
-      this.hideAddContactForm();
+      this.contactAdded = true;
+      setTimeout(() => {
+        this.hideAddContactForm();
+        this.contactAdded = false;
+      }, 1000);
     } catch (error: any) {
-      this.errorMessage = 'Fehler beim Speichern des Kontakts: ' + error.message;
       console.error('Fehler beim Speichern des Kontakts: ', error);
     } finally {
-      this.isLoading = false;
     }
   }
 
