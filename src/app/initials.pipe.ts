a9af10fc25ff37051818124c2f1593d04aa27a5e
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-04-09 15:48:39 +0200

    optimized initials.pipe

--- src/app/initials.pipe.ts
+++ src/app/initials.pipe.ts
@@ -7,9 +7,19 @@ export class InitialsPipe implements PipeTransform {
 
   transform(fullName: string): string {
     if (!fullName) return '';
-    const words = fullName.trim().split(/\s+/); // Teilt bei einem oder mehreren Leerzeichen
-    const initials = words.map(word => word[0]?.toUpperCase()).slice(0, 2).join(''); // slice: falls Doppelnamen oder mehrere Vornamen -> nur die ersten beiden Initialen
+    const words = fullName.trim().split(/\s+/); // trim() entfernt Leerzeichen vorne und hinten, split(/\s+/) teilt bei einem oder mehreren Leerzeichen
+    const initials = words.filter(word => !!word).map(word => word.charAt(0).toUpperCase()).slice(0, 2).join(''); // filter: leere Strings raus; map: holt sich den ersten Buchstaben jedes Wortes; slice: falls Doppelnamen oder mehrere Vornamen -> nur die ersten beiden Initialen; join: setzt die Buchstaben zusammen
     return initials;
+
+    //Falls in Zukunft zweiter Vorname bei Eingabe möglich ist, brauche ich dann eine geänderte Funktion:
+    // if (words.length === 0) return '';
+    // const firstInitial = words[0].charAt(0).toUpperCase();
+    // const lastInitial = words[words.length - 1].charAt(0).toUpperCase();
+    // if (words.length === 1) {
+      //return firstInitial; // Falls nur ein einzelner Name eingegeben wurde. (Das ist momentan nicht möglich.)
+      // } else {
+        // return firstInitial + lastInitial;
+      // }
   }
 
 }
