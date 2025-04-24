6128d447263354f0d83557c1ecf59bddb113f567
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-04-24 11:48:35 +0200

    subtasks progress including progress bar

--- src/app/components/board/task/task.component.ts
+++ src/app/components/board/task/task.component.ts
@@ -16,18 +16,13 @@ export class TaskComponent {
 
   @Input()task?: Task; //Das Fragezeichen macht das Property optional, die Komponente benötigt beim Start also nicht zwingend einen Wert für task. In Kombination mit @if (task) in der HTML-Datei wird verhindert, dass die Komponente versucht auf contact zuzugreifen, wenn es noch gar nicht gesetzt wurde.
 
-  // @Input()task = {
-  //   title: "HTML Base Template Creation",
-  //   description: "Create reusable HTML base templates.",
-  //   date: "",
-  //   priority: "Medium",
-  //   assigned: "Anita Gasteiner",
-  //   category: "Technical Task",
-  //   status: "to-do",
-  //   subtasks: {
-  //     subtask1: "Create template 1.",
-  //     subtask2: "Create template 2."
-  //   }
-  // };
+  subtasksDone: string = '0';
+
+  get progress(): number {
+    const total = this.task?.subtasks.length ?? 0;
+    const completed = this.task?.subtasks.filter(subtask => subtask.done).length ?? 0;
+    this.subtasksDone = `${completed}`;
+    return total > 0 ? (completed / total) * 100 : 0;    
+  }
 
 }
