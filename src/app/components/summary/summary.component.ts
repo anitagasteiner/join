03c1f5aa132d27fd6d29e67be7b8376463025a67
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-04-28 22:06:35 +0200

    choose summary greeting depending on daytime

--- src/app/components/summary/summary.component.ts
+++ src/app/components/summary/summary.component.ts
@@ -15,4 +15,20 @@ export class SummaryComponent {
     this.generalService.activeNavBtn = 'summary';
   }
 
+  chooseGreeting() {
+    let d = new Date();
+    let h = d.getHours();
+    if (h >= 4 && h < 10) {
+        return 'Good morning';
+    } else if ((h < 4) || (h >= 10 && h < 13)) {
+        return  'Hello';
+    } else if (h >= 13 && h < 17) {
+        return 'Good afternoon';
+    } else if (h >= 17) {
+        return 'Good evening';
+    } else {
+      return 'Hello';
+    }
+  }
+
 }
