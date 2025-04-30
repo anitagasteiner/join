5c71561c7dbf3d627cbf9951bad0ab567e80b592
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-04-30 11:03:26 +0200

    security measure

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,4 @@
 export const environment = {
     production: false,
     googleApiKey: 'AIzaSyB_idVWCPV1F3PJ-ksM6k6ssP2OmpYoL9Q'
-<<<<<<< HEAD
-  };
-  
-=======
 };
->>>>>>> 7424b935031db135f42e617af5bee34cbb7b2ec0
