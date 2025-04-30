7174da0d1defffccd3194b6dfeed2ef396884a8f
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-04-30 10:45:51 +0200

    api key security resolved

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    googleApiKey: 'AIzaSyB_idVWCPV1F3PJ-ksM6k6ssP2OmpYoL9Q'
+  };
+ 
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    googleApiKey: 'AIzaSyB_idVWCPV1F3PJ-ksM6k6ssP2OmpYoL9Q'
+  };
+  
