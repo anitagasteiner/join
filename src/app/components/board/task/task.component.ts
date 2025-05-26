b9c7a6ac0a9d0e8983ddb3ca66ffa471fc306132
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-05-26 10:45:25 +0200

    task component: jsdoc

--- src/app/components/board/task/task.component.ts
+++ src/app/components/board/task/task.component.ts
@@ -17,12 +17,26 @@ import { InitialsPipe } from '../../../pipes/initials.pipe';
 })
 export class TaskComponent {
 
-  @Input()task?: Task; //Das Fragezeichen macht das Property optional, die Komponente benötigt beim Start also nicht zwingend einen Wert für task. In Kombination mit @if (task) in der HTML-Datei wird verhindert, dass die Komponente versucht auf contact zuzugreifen, wenn es noch gar nicht gesetzt wurde.
+  /**
+   * The Task object passed into the component.
+   * This property is optional to allow safe rendering.
+   */
+  @Input()task?: Task; //NOTE - Das Fragezeichen macht das Property optional, die Komponente benötigt beim Start also nicht zwingend einen Wert für task. In Kombination mit @if (task) in der HTML-Datei wird verhindert, dass die Komponente versucht auf contact zuzugreifen, wenn es noch gar nicht gesetzt wurde.
 
+  /**
+   * Stores the number of completed subtasks in a string.
+   * It's updated every time the 'progress' getter is called.
+   * @type {string}
+   * @memberof TaskComponent
+   */
   subtasksDone: string = '0';
 
   constructor() { }
 
+  /**
+   * Calculates the percentage of completed subtasks.
+   * Updates the 'subtasksDone' property with the current number of completed subtasks.
+   */
   get progress(): number {
     const total = this.task?.subtasks.length ?? 0;
     const completed = this.task?.subtasks.filter(subtask => subtask.done).length ?? 0;
