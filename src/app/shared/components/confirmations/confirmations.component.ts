8954f6f7fbbbcf0f67893a6008f8fc9c7ade32be
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-05-30 14:50:40 +0200

    confirmations component: jsdoc

--- src/app/shared/components/confirmations/confirmations.component.ts
+++ src/app/shared/components/confirmations/confirmations.component.ts
@@ -12,15 +12,40 @@ import { ContactsService } from '../../../services/contacts.service';
 })
 export class ConfirmationsComponent {
 
-  generalService = inject(GeneralService);
-  tasksService = inject(TasksService);
-  contactsService = inject(ContactsService);
+  /**
+   * Instance of GeneralService used to interact with general data and operations.
+   * 
+   * @type {GeneralService}
+   */
+  generalService: GeneralService = inject(GeneralService);
 
-  closeConfirmationDeleteTask() {
+  /**
+   * Instance of TasksService used to manage task data and operations.
+   * 
+   * @type {TasksService}
+   */
+  tasksService: TasksService = inject(TasksService);
+
+  /**
+   * Instance of ContactsService used to manage contact data and operations.
+   * 
+   * @type {ContactsService}
+   */
+  contactsService: ContactsService = inject(ContactsService);
+
+  /**
+   * Closes the confirmation dialog for task deletion.
+   * Sets the 'confirmationDeleteTask' flag in the general service to 'false'.
+   */
+  closeConfirmationDeleteTask(): void {
     this.generalService.confirmationDeleteTask = false;
   }
 
-  closeConfirmationDeleteContact() {
+  /**
+   * Closes the confirmation dialog for contact deletion.
+   * Sets the 'confirmationDeleteContact' flag in the general service to 'false'.
+   */
+  closeConfirmationDeleteContact(): void {
     this.generalService.confirmationDeleteContact = false;
   }
 
